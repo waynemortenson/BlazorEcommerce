3bdde08bcc503fed05307d05ad60ddfabfd19461
wayne <waynemortenson@gmail.com>
2022-12-02 07:00:55 -0600

    Add project files.

---
+++ BlazorEcommerce/Shared/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorEcommerce.Shared.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
